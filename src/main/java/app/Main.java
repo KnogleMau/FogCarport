4b29f77311cf1e4775c1e269d57c55dedaed0e39
KnogleMau <mav.solver@hotmail.com>
2025-04-28 13:31:26 +0200

    Lavet den rigtige Mappe Struktur, så vi er klar til og arbejde.
    Lagt Thymeleaf, SessionConfig og ConnectionPool ind i de rigtige klasser
    
    Jeg har opdaterert Pom filen så den er up to date, med hvad vi har behov for i forhold til projektet

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,43 @@
 package app;
 
+import app.config.SessionConfig;
+import app.config.ThymeleafConfig;
+import app.controllers.CupcakeController;
+import app.controllers.UserController;
+import app.entities.Bottom;
+import app.entities.Cupcake;
+import app.entities.Topping;
+import app.persistence.BottomMapper;
+import app.persistence.ConnectionPool;
+
+import app.persistence.ToppingMapper;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
+import java.util.List;
+import java.util.logging.Logger;
+
 public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "cupcakes";
+
+    public static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        // Initializing Javalin and Jetty webserver
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.jetty.modifyServletContextHandler(handler -> handler.setSessionHandler(SessionConfig.sessionConfig()));
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+        }).start(7070);
+
+       // CupcakeController.routes(app, connectionPool);
     }
 }
