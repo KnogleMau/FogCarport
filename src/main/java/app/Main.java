e2e4f1d8be2b7cab7f924e7855fc651406d552b2
KnogleMau <mav.solver@hotmail.com>
2025-04-28 13:31:56 +0200

    Lavet den rigtige Mappe Struktur, så vi er klar til og arbejde.
    Lagt Thymeleaf, SessionConfig og ConnectionPool ind i de rigtige klasser
    
    Jeg har opdaterert Pom filen så den er up to date, med hvad vi har behov for i forhold til projektet

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,18 +2,9 @@ package app;
 
 import app.config.SessionConfig;
 import app.config.ThymeleafConfig;
-import app.controllers.CupcakeController;
-import app.controllers.UserController;
-import app.entities.Bottom;
-import app.entities.Cupcake;
-import app.entities.Topping;
-import app.persistence.BottomMapper;
 import app.persistence.ConnectionPool;
-
-import app.persistence.ToppingMapper;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
-
 import java.util.List;
 import java.util.logging.Logger;
 
