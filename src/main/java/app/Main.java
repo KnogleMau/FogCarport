dbac0ed7223fbb4dbbccc34a6cc27e98b7e1e473
KnogleMau <mav.solver@hotmail.com>
2025-05-02 11:07:13 +0200

    Ændret i main
    Gjort så når vi starter main, connector systemet faktisk til selve den online database vi har os
    
    Så vi har ændret til det her
    jdbc:postgresql://46.101.114.35:5432/%s?currentSchema=public
    fra
    jdbc:postgresql://localhost:5432/%s?currentSchema=public
    
    Det gør jeg fordi vi skal connect til en ip istedet for localhost da vi ikke længere hoster vores database

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -12,9 +12,9 @@ public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
-    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
-    private static final String DB = "cupcakes";
+    private static final String PASSWORD = "vinkelvej20";
+    private static final String URL = "jdbc:postgresql://46.101.114.35:5432/%s?currentSchema=public";
+    private static final String DB = "fogcarport";
 
     public static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
