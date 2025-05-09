e096ce17d024f9e8b1f570e82ae6ed53679cd1e4
KnogleMau <mav.solver@hotmail.com>
2025-05-09 11:45:10 +0200

    Igang med og lave enviroments, så man ikke ligger alt muligt op i github

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -6,6 +6,8 @@ import app.javaCode.Sendgrid;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -13,7 +15,7 @@ public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     private static final String USER = "postgres";
-    private static final String PASSWORD = "vinkelvej20";
+    private static final String PASSWORD = System.getenv("KODE");
     private static final String URL = "jdbc:postgresql://46.101.114.35:5432/%s?currentSchema=public";
     private static final String DB = "fogcarport";
 
@@ -30,6 +32,12 @@ public class Main {
             config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
         }).start(7070);
 
+        Sendgrid s = new Sendgrid();
+        try {
+            s.sendGridAction("johannesfogcarport@gmail.com", "Mr FOG", "d-1dc8cdf4e45d44139c6ac4711ea45adc");
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
 
 
        // CupcakeController.routes(app, connectionPool);
