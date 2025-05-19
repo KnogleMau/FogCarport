b1514d26e4e4a3d01f39d1168519e049ca230687
Istimereal <jjfapir@gmail.com>
2025-05-19 17:52:36 +0200

    Ordre, ordre detaljer og top view tegning kan ses i adminViewOrders  (for test_all databasen). Der er ingen styling. Der mangler rute tilbage til adminMenu

--- src/main/java/app/controllers/AdminUserController.java
+++ src/main/java/app/controllers/AdminUserController.java
@@ -24,7 +24,9 @@ public class AdminUserController {
       ctx.render("AdminMainMenu");
         });
 
-        app.post("carport-top-svg", ctx -> showDrawingAtOrders(ctx, connectionPool));
+        app.post("/displayOrderDetails", ctx -> showDrawingAtOrders(ctx, connectionPool));
+
+       // app.post("carport-top-svg", ctx -> showDrawingAtOrders(ctx, connectionPool));
 
         app.get("/adminViewOrders", ctx -> displayAllOrders(ctx, connectionPool));
 
@@ -35,15 +37,23 @@ public class AdminUserController {
         try {
             System.out.println("displayAllOrders m1");
             ArrayList<OrderCustomerDTO> allOrders = OrderAndDetailsDTOMapper.getAllOrderCustomerDTOsMapper();
-            System.out.println("displayAllOrders m2");
-            ctx.sessionAttribute("allOrders", allOrders);
+        //    System.out.println("displayAllOrders m2");
+            for(OrderCustomerDTO order : allOrders){
+                System.out.println(order.getEmail() +" " +order.getFirstName() +" "+ order.getLastName() +" "+  order.getOrderID() +" "+ order.getOrderStatus() +" "+order.getPhoneNumber()+" "+ order.getOrderID()+"\n");
+
+
+            }
+           ctx.attribute("allOrders", allOrders);
+
             System.out.println("displayAllOrders m3");
             ctx.render("adminViewOrders");
             System.out.println("displayAllOrders m4");
+
         }
         catch (DatabaseException e ){
 
             ctx.attribute("message", "Der er et problem med databasen.");
+            ctx.render("/adminViewOrders");
         }
     }
 
@@ -55,12 +65,9 @@ public class AdminUserController {
 
     public static void showDrawingAtOrders(Context ctx, ConnectionPool connectionPool){
         Locale.setDefault(new Locale("US")); // Makes sure that decimals are displayed with . instead of so it can be read by the SVG templates
-// skal evt laves så det hele ligger her og bliver kaldt med eller uden  topViewCarportSVG
-        // afhængigt af kaldet
-        //    int width = 240;
-        //   int height = 300;
-      //  double length = 450;
-      //  double width = 300;
+
+        double length = 0;
+        double width = 0;
 
         String materialName;
         int materialLength;
@@ -68,18 +75,51 @@ public class AdminUserController {
         String materialUnit;
         String materialDescription;
 
-        int orderId = Integer.parseInt(ctx.sessionAttribute("orderId"));
+        int orderId = Integer.parseInt(ctx.formParam("orderId"));
+        System.out.println("showDrawingAtOrders m1 orderID: " + orderId);
         try {
             ArrayList<OrderDetailsMaterialLengthDTO> detailsDTO = OrderAndDetailsDTOMapper.getOrderDetailsMapper(orderId);
-            double length = 0;
-           // CarportSVG topViewCarportSVG = new CarportSVG(length, width);
-          //  ctx.attribute("svg", topViewCarportSVG.toString());
-            ctx.sessionAttribute("detailsDTO",detailsDTO);
+        for(OrderDetailsMaterialLengthDTO dto : detailsDTO){
+
+            if(dto.getMaterialDescription().substring(0,1).toLowerCase().equals("s"));{
+                   width = dto.getMaterialLength(); }
+            if(dto.getMaterialDescription().substring(0,1).toLowerCase().equals("r")) {
+                   length = dto.getMaterialLength();
+            }
+
+        }
+
+            CarportSVG topViewCarportSVG = new CarportSVG(length, width);
+            ctx.attribute("svg", topViewCarportSVG.toString());
+            System.out.println("detailsDTO: " + detailsDTO.size() +" "+ detailsDTO.get(0).getMaterialDescription() +" " + detailsDTO.get(1).getMaterialLength());
+            ctx.attribute("detailsDTO", detailsDTO);
             ctx.render("adminViewOrders.html");
         }
         catch (DatabaseException e){
             ctx.attribute("message", "Der er et problem med databasen.");
         }
+    }
+
+    public void showDrawings(Context ctx, ConnectionPool connectionPool){
+        Locale.setDefault(new Locale("US")); // Makes sure that decimals are displayed with . instead of so it can be read by the SVG templates
+// skal evt laves så det hele ligger her og bliver kaldt med eller uden  topViewCarportSVG
+        // afhængigt af kaldet
+        //    int width = 240;
+        //   int height = 300;
+        //  double length = 450;
+        //  double width = 300;
+        String materialName;
+        int materialLength;
+        int quantity;
+        String materialUnit;
+        String materialDescription;
+
+        int orderId = Integer.parseInt(ctx.formParam("orderId"));
+
+        double width = Double.parseDouble(ctx.formParam("width"));
+        double height = Double.parseDouble(ctx.formParam("height"));
+
+
     }
 }
 
